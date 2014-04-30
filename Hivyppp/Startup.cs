35cb492f6248d0fade1429f54e162d23cc9f7b1b
Craig <craig.williams@acutesales.co.uk>
2014-04-30 22:59:17 +0100

    initial commit

---
+++ Hivyppp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hivyppp.Startup))]
+namespace Hivyppp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
